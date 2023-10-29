66090ba2ac3de7fc8a1326adb538c0774676db50
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-30 00:59:09 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -51,14 +51,11 @@ echo 100
 echo "https://www.apkmirror.com$url3" >&2
 echo "Downloading APK from: https://www.apkmirror.com$url3"
 
-        # Download the APK file and save it as snap.apk
-        wget -U "$UserAgent" -O snap.apk "https://www.apkmirror.com$url3"
-        if [ $? -eq 0 ]; then
-            echo "APK downloaded successfully as snap.apk"
-        else
-            echo "Failed to download APK" >&2
-            exit 1
-        fi
-    fi
-done
-
+# Download the APK file and save it as snap.apk
+wget -U "$UserAgent" -O snap.apk "https://www.apkmirror.com$url3"
+if [ $? -eq 0 ]; then
+    echo "APK downloaded successfully as snap.apk"
+else
+    echo "Failed to download APK" >&2
+    exit 1
+fi
