7b60690fa3780cdbe9f19550c91033531f6bb54d
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-30 00:26:26 +0500

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -4,12 +4,17 @@ UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, 
 
 page1=$(curl --fail-early --connect-timeout 2 --max-time 5 -sL -A "$UserAgent" "https://www.apkmirror.com/uploads/?appcategory=Snapchat" 2>&1)
 readarray -t versions < <(pup -p 'div.widget_appmanager_recentpostswidget h5 a.fontBlack text{}' <<<"$page1")
-    for version in "${versions[@]}"; do
-        if [[ ! "$version" == *"Beta" ]] && [[ ! "$version" == *"beta" ]]; then
-            echo "$version"
-            exit 0
-        fi
-    done
-    echo "No version without 'Beta' found."
-    exit 1
-fi
+
+for version in "${versions[@]}"; do
+    if [[ ! "$version" == *"Beta" ]] && [[ ! "$version" == *"beta" ]]; then
+        # Extract version number and replace spaces and dots with hyphens
+        version=$(echo "$version" | tr -s ' ' | tr '.' '-')
+        # Add app name as a prefix
+        version="[snapchat-$version]"
+        echo "$version"
+        exit 0
+    fi
+done
+
+echo "No version without 'Beta' found."
+exit 1
