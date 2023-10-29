d01ea7313ed108986f9d35cec11e2cece880d5a4
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-30 00:08:35 +0500

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -3,10 +3,11 @@
 UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 
 apkmirrorAppName="$1"
+expectedURL="https://www.apkmirror.com/uploads/?appcategory=Snapchat"
 
-page1=$(curl --fail-early --connect-timeout 2 --max-time 5 -sL -A "$UserAgent" "https://www.apkmirror.com/uploads/?appcategory=$apkmirrorAppName" 2>&1)
+page1=$(curl --fail-early --connect-timeout 2 --max-time 5 -sL -A "$UserAgent" "$expectedURL" 2>&1)
 
-if [ "$page1" == "https://www.apkmirror.com/uploads/?appcategory=$apkmirrorAppName" ]; then
+if [[ "$page1" == *"$expectedURL"* ]]; then
     readarray -t versions < <(pup -p 'div.widget_appmanager_recentpostswidget h5 a.fontBlack text{}' <<<"$page1")
 
     for version in "${versions[@]}"; do
