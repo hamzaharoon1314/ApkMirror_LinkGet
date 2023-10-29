d2957e431e2706f0a43207d4d1ea34f0f95ad175
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-29 23:58:03 +0500

    Create getVersion.sh

---
+++ getVersion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+
+apkmirrorAppName="$1"
+
+page1=$(curl --fail-early --connect-timeout 2 --max-time 5 -sL -A "$UserAgent" "https://www.apkmirror.com/uploads/?appcategory=$apkmirrorAppName" 2>&1)
+
+if [ "$page1" == "https://www.apkmirror.com/uploads/?appcategory=$apkmirrorAppName" ]; then
+    readarray -t versions < <(pup -p 'div.widget_appmanager_recentpostswidget h5 a.fontBlack text{}' <<<"$page1")
+
+    for version in "${versions[@]}"; do
+        if [[ ! "$version" == *"beta"* ]]; then
+            echo "$version"
+            exit 0
+        fi
+    done
+    echo "No version without 'beta' found."
+    exit 1
+else
+    echo "Error: Page URL is not as expected."
+    exit 1
+fi
