f91f8f0bf3e41c898d2c14cd5eebad233d57f8bd
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-30 00:21:35 +0500

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -5,9 +5,8 @@ UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, 
 page1=$(curl --fail-early --connect-timeout 2 --max-time 5 -sL -A "$UserAgent" "https://www.apkmirror.com/uploads/?appcategory=Snapchat" 2>&1)
 
 # Check if the HTML content contains a specific keyword or phrase that indicates it's the correct page
-if [[ "$page1" == *"Snapchat related uploads"* ]]; then
-    readarray -t versions < <(pup -p 'div.widget_appmanager_recentpostswidget h5 a.fontBlack text{}' <<<"$page1")
-
+readarray -t versions < <(pup -p 'div.widget_appmanager_recentpostswidget h5 a.fontBlack text{}' <<<"$page1")
+echo "$readarray"
     for version in "${versions[@]}"; do
         if [[ ! "$version" == *"Beta" ]] && [[ ! "$version" == *"beta" ]]; then
             echo "$version"
@@ -15,8 +14,5 @@ if [[ "$page1" == *"Snapchat related uploads"* ]]; then
         fi
     done
     echo "No version without 'Beta' found."
-    exit 1
-else
-    echo "Error: Page content is not as expected."
     exit 1
 fi
