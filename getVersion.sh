4427d9d284a373611748a7864235839ddf1939ee
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-30 00:22:19 +0500

    Update getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -3,10 +3,7 @@
 UserAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 
 page1=$(curl --fail-early --connect-timeout 2 --max-time 5 -sL -A "$UserAgent" "https://www.apkmirror.com/uploads/?appcategory=Snapchat" 2>&1)
-
-# Check if the HTML content contains a specific keyword or phrase that indicates it's the correct page
 readarray -t versions < <(pup -p 'div.widget_appmanager_recentpostswidget h5 a.fontBlack text{}' <<<"$page1")
-echo "$readarray"
     for version in "${versions[@]}"; do
         if [[ ! "$version" == *"Beta" ]] && [[ ! "$version" == *"beta" ]]; then
             echo "$version"
