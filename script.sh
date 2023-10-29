04d983d0494e3fd6b8e3b6ad16389ea504cf554b
Hamza Haroon <hamzaharoon1314@gmail.com>
2023-10-30 00:55:36 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -52,7 +52,7 @@ echo "https://www.apkmirror.com$url3" >&2
 echo "Downloading APK from: https://www.apkmirror.com$url3"
 
         # Download the APK file and save it as snap.apk
-        wget -O snap.apk "https://www.apkmirror.com$url3"
+        wget -U "$UserAgent" -O snap.apk "https://www.apkmirror.com$url3"
         if [ $? -eq 0 ]; then
             echo "APK downloaded successfully as snap.apk"
         else
